6c337da9100396a5167e0fce00e1c3b3874c60d2
mirangee <pmrang@live.co.kr>
2024-03-22 16:47:49 +0900

    스프링 요청 처리 및 jsp 연결

--- README.md
+++ README.md
@@ -7,5 +7,21 @@
   implementation 'org.apache.tomcat.embed:tomcat-embed-jasper'
   implementation 'jakarta.servlet:jakarta.servlet-api'
   implementation 'jakarta.servlet.jsp.jstl:jakarta.servlet.jsp.jstl-api'
-  implementation 'org.glassfish.web:jakarta.servlet.jsp.jstl'
+  implementation 'org.glassfish.web:jakarta.servlet.jsp.jstl'~~~~
   ```
+  
+<br>
+
+- application.properties 파일에 추가(아래 2가지)
+
+포트 번호 변경
+```
+# server port change
+server.port =8181
+```
+외부에서 jsp 파일을 지목하지 못하도록 WEB-INF 폴더에 넣어 놓을 것이며, 파일은 .jsp로 끝날 것이라고 Spring에게 알려주는 세팅
+```
+# jsp view resolver setting
+spring.mvc.view.prefix=/WEB-INF/views/
+spring.mvc.view.suffix=.jsp
+```
---
+++ src/main/java/com/spring/mvc/chap01/ControllerV1.java
@@ -0,0 +1,65 @@
+package com.spring.mvc.chap01;
+
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+// 컨트롤러: 클라이언트의 요청을 받아서 처리 후 응답을 결정하는 역할
+@Controller // @Component로 해도 되지만 controller 역할을 할 것이므로 @Controller로 빈 등록한다.
+// 이 클래스의 객체 생성 및 관리는 스프링 컨테이너가 처리한다.
+public class ControllerV1 {
+
+    // Spring에서는 메서드 별로 url을 매핑하여 처리한다. 이전에는 jsp에서 확장자 패턴을 활용해 mapping했던 것과 비교되는 방법..
+//    @RequestMapping(value = "/food", method = RequestMethod.GET)
+    @GetMapping("/food")
+    public String food() {
+        // 리턴문에는 어떤 jsp로 포워딩할지 경로를 적습니다.
+        // 경로를 작성할 때는 view resolver 세팅에 작성된 접두어, 접미어를 고려하여 씁니다.
+        // 현재 application.properties 파일을 보면 view resolver에 접두어 /WEB-INF/views/, 접미어 .jsp로 세팅되어 있음.
+        // 그러므로 경로 /WEB-INF/views/뭐시기.jsp 중 뭐시기 부분을 return 해주면 된다.
+        return "chap01/food";
+    }
+
+    // ==================== 요청 파라미터 읽기 (클라이언트가 보낸 데이터) ======================
+    // 1. HttpServletRequest 객체 이용하기 -> 전통적 방식
+
+
+    // URL이 같아도 전송 요청 방식에 따라 다르게 처리를 할 수 있다.
+//    @RequestMapping(value = "/food", method = RequestMethod.POST)
+    // RequestMapping을 아래와 같이 간단하게 바꿀 수 있음.
+    /*
+    @PostMapping("/food")
+    public String foodReg(HttpServletRequest request) {
+        String foodName = request.getParameter("foodName");
+        String category = request.getParameter("category");
+        System.out.println("foodName = " + foodName);
+        System.out.println("category = " + category);
+        return null;
+    }
+    */
+
+    // 2. @RequestParam 사용하기
+    // 매개변수로 '@RequestParam("파라미터 이름") 이 값을 받을 매개변수'를 작성한다.
+    // 1번 방법은 getParameter했을 시 무조건 문자열로 반환되어서 경우에 따라 ParseInt 등을 해야 했다.
+    // 하지만 이 방식은 내가 원하는 타입을 미리 지정 가능해 타입변환할 필요가 없다.
+    // 심지어, 파라미터 이름과 매개변수 이름이 같으면 @RequestParam을 아예 생략할 수 있다.
+    /*  
+    @PostMapping("/food")
+    public String food(@RequestParam("foodName") String name,
+                       String category,
+                       int price) {
+        System.out.println("name = " + name);
+        System.out.println("category = " + category);
+        System.out.println("price = " + price);
+        return null;
+    }
+    */
+    
+    // 3. DTO (Data Transfer Object) 객체 사용 -> 커맨드 객체를 활용한 파라미터 처리
+    // 처리해야 할 파라미터가 많거나 혹은 서로 연관되어 있는 데이터인 경우 사용한다.
+    @PostMapping("/food")
+    public String food(FoodOrderDTO orderDTO){
+        System.out.println("orderDTO = " + orderDTO);
+        return null;
+    }
+}
